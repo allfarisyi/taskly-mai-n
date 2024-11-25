bffecc2f0251acb1a39b0f54cec532e8ff640640
allfarisyi <aldialfarisyi1@gmail.com>
2024-11-25 16:44:17 +0700

    chapter 8

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,10 @@
+import { Box, Heading, Button } from '@chakra-ui/react';
+
 export default function Home() {
-    return <h1>Home</h1>;
-  }
-  
+return (
+    <Box p='4'>
+      <Heading>Home</Heading>
+      <Button color scheme='teal'>Click Me</Button>
+    </Box>
+  );
+}
