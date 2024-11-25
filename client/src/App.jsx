1691b790cadccdf60b64317713f3bca5d3720e4a
allfarisyi <aldialfarisyi1@gmail.com>
2024-11-25 12:16:03 +0700

    chapter 8

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import { system } from "@chakra-ui/react/preset";
+
+import { Toaster } from 'react-hot-toast'
+
 export default function App() {
   return (
+  <ChakraProvider value={system}>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
@@ -11,5 +17,6 @@ export default function App() {
         <Route path='/signup' element={<SignUp />} />
       </Routes>
     </BrowserRouter>
+  </ChakraProvider>
   );
 }
